fbfdbb879dcb25a49b03657ca518cf6f6f8b12a8
Keenan <kkan68@gmail.com>
2021-11-30 02:00:57 +0000

    fixed list function a little

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let domain = [".com", ".net"];
-let domainName = [];
 
 window.onload = function() {
+  let domainName = [];
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
