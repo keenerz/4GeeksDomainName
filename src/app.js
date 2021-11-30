9db97c06b044b3043b84129f84ef6dcfd3e3f1a2
Keenan <kkan68@gmail.com>
2021-11-30 01:51:03 +0000

    added input

--- src/app.js
+++ src/app.js
@@ -2,22 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-let pronoun = ["the", "our", "how", "bog"];
-let adj = ["great", "big", "tall", "short"];
-let noun = ["jogger", "racoon", "wizard", "pokemon"];
-let domain = [".com", ".us", ".net", ".org"];
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".net"];
 let domainName = [];
 
 window.onload = function() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      for (let k = 0; k < 4; k++) {
-        for (let l = 0; l < 4; l++) {
-          let basedName = pronoun[i] + adj[j] + noun[k] + domain[l];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          let basedName = " " + pronoun[i] + adj[j] + noun[k] + domain[l];
           domainName.push(basedName);
         }
       }
     }
   }
   console.log(domainName);
+  document.getElementById("domain.results").innerHTML = domainName;
 };
+
+let btnAdd = document.querySelector("#add");
+let input = document.querySelector("input");
+
+btnAdd.addEventListener("click", () => {
+  if (input == null) {
+    return;
+  } else {
+    pronoun.push(input.value);
+    input.value = "";
+    window.onload();
+  }
+});
