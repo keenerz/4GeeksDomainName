8f74901c4fe49ca191e7782b09008ea1d48c93bf
Keenan <kkan68@gmail.com>
2021-11-30 01:16:35 +0000

    major changes

--- src/app.js
+++ src/app.js
@@ -6,20 +6,18 @@ let pronoun = ["the", "our", "how", "bog"];
 let adj = ["great", "big", "tall", "short"];
 let noun = ["jogger", "racoon", "wizard", "pokemon"];
 let domain = [".com", ".us", ".net", ".org"];
+let domainName = [];
 
 window.onload = function() {
-  for (let i = 0; i < 8; i++) {
-    let pronounIndex = Math.floor(Math.random() * pronoun.length);
-    let adjIndex = Math.floor(Math.random() * adj.length);
-    let nounIndex = Math.floor(Math.random() * noun.length);
-    let domainIndex = Math.floor(Math.random() * domain.length);
-
-    let domainName =
-      pronoun[pronounIndex] +
-      adj[adjIndex] +
-      noun[nounIndex] +
-      domain[domainIndex];
-
-    console.log(domainName);
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      for (let k = 0; k < 4; k++) {
+        for (let l = 0; l < 4; l++) {
+          let basedName = pronoun[i] + adj[j] + noun[k] + domain[l];
+          domainName.push(basedName);
+        }
+      }
+    }
   }
+  console.log(domainName);
 };
