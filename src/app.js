85a534278be7eaab9ea74da078990a7d08491f2b
Keenan <kkan68@gmail.com>
2021-11-30 00:43:19 +0000

    basic finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our", "how", "bog"];
+let adj = ["great", "big", "tall", "short"];
+let noun = ["jogger", "racoon", "wizard", "pokemon"];
+let domain = [".com", ".us", ".net", ".org"];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < 8; i++) {
+    let pronounIndex = Math.floor(Math.random() * pronoun.length);
+    let adjIndex = Math.floor(Math.random() * adj.length);
+    let nounIndex = Math.floor(Math.random() * noun.length);
+    let domainIndex = Math.floor(Math.random() * domain.length);
+
+    let domainName =
+      pronoun[pronounIndex] +
+      adj[adjIndex] +
+      noun[nounIndex] +
+      domain[domainIndex];
+
+    console.log(domainName);
+  }
 };
